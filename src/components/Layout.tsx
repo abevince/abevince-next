3020d88026bf11ae18d65fb33c8095911b36b74a
Vincent Abesamis <vincentabesamis03@gmail.com>
2022-08-09 22:52:00 +0800

    Add title

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import Link from 'next/link'
 import * as React from 'react'
 import MobileNav from './MobileNav'
@@ -26,36 +27,42 @@ const DesktopNav = () => (
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="w-full min-h-screen flex items-center bg-neutral-900 bg-[url('/texture.png')] bg-repeat py-8 flex-col text-neutral-200">
-      <div className="flex flex-row w-full lg:w-[1024px] justify-between items-center px-4">
-        <div className="font-mono text-xl tracking-wide font-medium">
-          <Link href="/">
-            <a>{`{abevince}`}</a>
-          </Link>
+    <>
+      <Head>
+        <title>Home | Vincent Abesamis</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <div className="w-full min-h-screen flex items-center bg-neutral-900 bg-[url('/texture.png')] bg-repeat py-8 flex-col text-neutral-200">
+        <div className="flex flex-row w-full lg:w-[1024px] justify-between items-center px-4">
+          <div className="font-mono text-xl tracking-wide font-medium">
+            <Link href="/">
+              <a>{`{abevince}`}</a>
+            </Link>
+          </div>
+          <DesktopNav />
+          <div className="flex flex-row justify-between items-center gap-2">
+            <MobileNav />
+            <button className="min-w-min backdrop-blur-sm bg-zinc-600/30 px-4 py-2 rounded-full border-2 border-neutral-700">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth={2}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+                />
+              </svg>
+            </button>
+          </div>
         </div>
-        <DesktopNav />
-        <div className="flex flex-row justify-between items-center gap-2">
-          <MobileNav />
-          <button className="min-w-min backdrop-blur-sm bg-zinc-600/30 px-4 py-2 rounded-full border-2 border-neutral-700">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth={2}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
-              />
-            </svg>
-          </button>
-        </div>
+        {children}
       </div>
-      {children}
-    </div>
+    </>
   )
 }
 
--- src/components/PageLayout.tsx
+++ src/components/PageLayout.tsx
@@ -1,11 +1,18 @@
+import Head from 'next/head'
 import * as React from 'react'
 
 const PageLayout = ({ children, title }: { children: React.ReactNode; title: string }) => {
   return (
-    <div className="w-full lg:w-[1024px] px-4 py-16 flex flex-col gap-8 mx-auto">
-      <h2 className="text-2xl font-medium">{title}</h2>
-      {children}
-    </div>
+    <>
+      <Head>
+        <title>{title} | Vincent Abesamis</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <div className="w-full lg:w-[1024px] px-4 py-16 flex flex-col gap-8 mx-auto">
+        <h2 className="text-2xl font-medium">{title}</h2>
+        {children}
+      </div>
+    </>
   )
 }
 

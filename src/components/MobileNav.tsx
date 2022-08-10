8e3676f49386d8b32be63c9c5af67f909861af6b
Vincent Abesamis <vincentabesamis03@gmail.com>
2022-08-10 08:19:27 +0800

    Fix mobile nav index

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -19,7 +19,7 @@ const MyLink = (
 const MenuLink = React.forwardRef(MyLink)
 
 const MobileNav = () => (
-  <Menu as="div" className="relative inline-block text-left md:hidden">
+  <Menu as="div" className="relative inline-block text-left md:hidden z-10">
     <div>
       <Menu.Button className="inline-flex justify-center items-center gap-1 min-w-min backdrop-blur-sm bg-zinc-600/30 px-4 py-2 rounded-full border-2 border-neutral-700">
         Menu
@@ -47,7 +47,7 @@ const MobileNav = () => (
     >
       <Menu.Items
         as="section"
-        className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-sm shadow-neutral-700 backdrop-blur-sm bg-zinc-600/30 border-2 border-neutral-700 border-opacity-5 focus:outline-none"
+        className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-sm shadow-neutral-700 backdrop-blur-md bg-zinc-600/30 border-2 border-neutral-700 border-opacity-5 focus:outline-none"
       >
         <div className="py-1">
           <Menu.Item>
